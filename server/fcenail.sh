7c3893192cd76e7d8c7bd60fda75a19f902fe5dd
J_Cat <joe@cataford.com>
2020-11-24 08:14:33 -0500

    Packaging for distribution.

--- server/fcenail.sh
+++ server/fcenail.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
-if [ ! -f ~/.fcenail ]; then
-  mkdir ~/.fcenail
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+INSTALL=
+case $key in
+    -i|--install)
+    INSTALL=true
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+SAVE_DIR=$PWD
+
+if [ "$INSTALL" = "true" ]; then
+  if [ ! -f /lib/systemd/system/fcenail.service ]; then
+    cp /usr/lib/node_modules/fcenail-server/fcenail.service /lib/systemd/system
+    systemctl enable fcenail.service
+  fi
+
+  if [ ! -d ~/.fcenail ]; then
+    mkdir ~/.fcenail
+  fi
+
+  cd ~/.fcenail
+
+  if [ ! -f ~/.fcenail/.env ]; then
+    /usr/lib/node_modules/fcenail-server/generate-environment.sh
+  fi
+
+  systemctl start fcenail.service
+  exit 0
 fi
 
 NODE=$(whereis node | sed -E 's/^.*: ([^ ]+).*$/\1/gi')
-
-SAVE_DIR=$PWD
-cd ~/.fcenail
-
-if [ ! -f /usr/lib/node_modules/fcenail-server/.env ]; then
-  ./generate-environment.sh
-fi
-
 $NODE /usr/lib/node_modules/fcenail-server/dist/server.js
-
 
 cd $SAVE_DIR
