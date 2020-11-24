1c6f19d744600aa4a9169261c82e4edd640d5bfa
J_Cat <joe@cataford.com>
2020-11-24 11:17:44 -0500

    Packaging for distribution.

--- server/fcenail.sh
+++ server/fcenail.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 SAVE_DIR=$PWD
-NODE_MODULES=$(whereis npm | sed -E 's/^.*: ([^ ]+)\/bin\/npm.*$/\1/gi')/lib/node_modules
-NODE=$(whereis node | sed -E 's/^.*: ([^ ]+).*$/\1/gi')
 
 cd ~/.fcenail
 
 if [ ! -f ~/.fcenail/.env ]; then
   echo 'Generating private key/certificate pair on first launch ...'
-  $NODE_MODULES/fcenail/generate-environment.sh
+  $NODE_PATH/lib/node_modules/fcenail/generate-environment.sh
 fi
 
-$NODE $NODE_MODULES/fcenail/dist/server.js
+$NODE_PATH/bin/node $NODE_PATH/lib/node_modules/fcenail/dist/server.js
 
 cd $SAVE_DIR
