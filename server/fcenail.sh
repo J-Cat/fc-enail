e24e24f8cae3bbc78c8a6eb799d394c8ba602928
J_Cat <joe@cataford.com>
2020-12-08 15:01:34 -0500

    Publishing to NPM.

--- server/fcenail.sh
+++ server/fcenail.sh
@@ -3,6 +3,12 @@ SAVE_DIR=$PWD
 
 cd ~/.fcenail
 
+CURRENT_VERSION=$(node -e "console.log(require('/usr/local/lib/nodejs/current/lib/node_modules/fcenail/package.json').version)")
+AVAILABLE_VERSION=$(npm view fcenail dist-tags.next)
+if [ "$CURRENT_VERSION" != "$AVAILABLE_VERSION" ]; then
+  npm install -g fcenail --unsafe
+fi
+
 if [ ! -f ~/.fcenail/.env ]; then
   echo 'Generating private key/certificate pair on first launch ...'
   $NODE_PATH/lib/node_modules/fcenail/generate-environment.sh
