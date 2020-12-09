0df57e36d7848add5a7f1fe3e79e6556f05f4ba4
J_Cat <joe@cataford.com>
2020-12-08 19:57:19 -0500

    Added ability to upload custom sounds.

--- server/fcenail.sh
+++ server/fcenail.sh
@@ -6,7 +6,7 @@ cd ~/.fcenail
 CURRENT_VERSION=$(node -e "console.log(require('/usr/local/lib/nodejs/current/lib/node_modules/fcenail/package.json').version)")
 AVAILABLE_VERSION=$(npm view fcenail dist-tags.next)
 if [ "$CURRENT_VERSION" != "$AVAILABLE_VERSION" ]; then
-  npm install -g fcenail --unsafe
+  npm install -g fcenail@next --unsafe
 fi
 
 if [ ! -f ~/.fcenail/.env ]; then
