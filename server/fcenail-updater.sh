4be757b4b23704585c4bfeb99622f72653e281ab
J_Cat <joe@cataford.com>
2020-12-20 08:25:32 -0500

    Working on updater service.

--- server/fcenail-updater.sh
+++ server/fcenail-updater.sh
@@ -6,7 +6,13 @@ cd ~/.fcenail
 CURRENT_VERSION=$(node -e "console.log(require('/usr/local/lib/nodejs/current/lib/node_modules/fcenail/package.json').version)")
 AVAILABLE_VERSION=$(npm view fcenail dist-tags.next)
 if [ "$CURRENT_VERSION" != "$AVAILABLE_VERSION" ]; then
+  echo "Installing new version: $AVAILABLE_VERSION."
   npm install -g fcenail@next --unsafe
+else
+  echo "Current version: $CURRENT_VERSION"
+  echo
+  echo "No updates available."
+  echo
 fi
 
 cd $SAVE_DIR
