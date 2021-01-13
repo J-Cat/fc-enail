1b43273c7da7d54854fc3640f1ff187ac85284c2
J_Cat <joe@cataford.com>
2021-01-12 22:35:54 -0500

    Fixed build script

--- build.sh
+++ build.sh
@@ -11,7 +11,22 @@ yarn build
 cd ../server
 cp ../README.md .
 
-yarn pack
+read -p "Generate package file (F), publish to NPM (P), or skip (N) [N] " PUBLISH
+
+PUBLISH=${PUBLISH:="N"}
+case $PUBLISH in
+  F | f)
+    yarn pack
+    ;;
+  P | p)
+    CURRENT_VERSION=$(node -e "console.log(require('./package.json').version)")
+    yarn publish --tag beta --silent
+    npm dist-tag add fcenail@$CURRENT_VERSION next
+    ;;
+esac
+if [[ "${PUBLISH,,}" =~ ^(n|yes)$ ]]; then
+
+#yarn pack
 #yarn publish --tag beta
 
 rm ./README.md
