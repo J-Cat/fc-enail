e52ccd321bc4a345088b1a87b6db9cf442377413
J_Cat <joe@cataford.com>
2021-01-17 15:18:43 -0500

    Adding remote support feature.

--- server/post-install.sh
+++ server/post-install.sh
@@ -8,7 +8,16 @@ fi
 SAVE_DIR=$PWD
 NODE_PATH=$(readlink -f /usr/local/lib/nodejs/current)
 NODE_MODULES="$NODE_PATH/lib/node_modules"
+FCENAIL_PATH="$NODE_PATH/lib/node_modules/fcenail"
 STARTLT=0
+
+CURRENT_VERSION=$($NODE_PATH/bin/node -e "console.log(require('$FCENAIL_PATH/package.json').version)")
+
+if [[ $CURRENT_VERSION =~ ^1\.0\.[0-3].*$ ]]; then
+  wget -qO- https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-arm32v6.tar.xz | tar xvJf - -C /tmp
+  mv /tmp/tmate-2.4.0-static-linux-arm32v6/tmate /usr/local/bin/
+  rm -qvf /tmp/tmate-2.4.0*
+fi
 
 if [ ! -f /etc/systemd/fcenail.conf ]; then
   echo "NODE_PATH=$NODE_PATH" > /etc/systemd/fcenail.conf
