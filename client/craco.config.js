cdce46845e69763bf0b1fcee037f277bce94df7e
J_Cat <joe@cataford.com>
2022-02-14 05:45:34 -0500

    Upgrading packages to fix dependabot security vulnerabilities.

--- client/craco.config.js
+++ client/craco.config.js
@@ -4,6 +4,9 @@ const CracoLessPlugin = require('craco-less');
 
 // eslint-disable-next-line no-undef
 module.exports = {
+  eslint: {
+    enable: false
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
